ee18c865949f68d63ab8e340ea52f9a913de2bfa
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 19:46:32 +0300

    Update ErrorResponse.java

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -2,11 +2,9 @@ package ru.practicum.moviehub.api;
 
 public class ErrorResponse {
     private final String error;
-    
     public ErrorResponse(String error) {
         this.error = error;
     }
-    
     public String getError() {
         return error;
     }
