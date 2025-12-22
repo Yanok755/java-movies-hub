e5df4458ee4bcdd93484a4e49b4a8ea335ef171a
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 19:39:24 +0300

    Update BaseHttpHandler.java

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -11,34 +11,21 @@ import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHttpHandler implements HttpHandler {
     protected static final String CT_JSON = "application/json; charset=UTF-8";
-    protected final Gson gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .create();
+    protected final Gson gson = new GsonBuilder().create();
 
     protected void sendJson(HttpExchange exchange, int statusCode, Object response) throws IOException {
         String jsonResponse = gson.toJson(response);
         byte[] bytes = jsonResponse.getBytes(StandardCharsets.UTF_8);
-
+        
         exchange.getResponseHeaders().set("Content-Type", CT_JSON);
         exchange.sendResponseHeaders(statusCode, bytes.length);
-
+        
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(bytes);
         }
     }
 
     protected void sendNoContent(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().set("Content-Type", CT_JSON);
         exchange.sendResponseHeaders(204, -1);
-    }
-
-    protected void sendText(HttpExchange exchange, int statusCode, String text) throws IOException {
-        byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=UTF-8");
-        exchange.sendResponseHeaders(statusCode, bytes.length);
-
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(bytes);
-        }
     }
 }
