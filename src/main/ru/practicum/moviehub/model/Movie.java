df1dca0056f793aefc5420d4c3ec5782ba8a36b9
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 18:24:42 +0300

    Update Movie.java

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,5 +1,27 @@
 package ru.practicum.moviehub.model;
 
 public class Movie {
+    private final String title;
+    private final int year;
+    private final int duration;
 
+    public Movie(String title, int year, int duration) {
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public int getYear() { return year; }
+    public int getDuration() { return duration; }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", duration=" + duration +
+                '}';
+    }
 }
