ce6963cb2f6c2409e5251c0080fd4cd58cef57bb
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 18:26:15 +0300

    Update BaseHttpHandler.java

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -1,7 +1,36 @@
 package ru.practicum.moviehub.http;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHttpHandler implements HttpHandler {
+    protected static final String CT_JSON = "application/json; charset=UTF-8";
+    protected final Gson gson = new GsonBuilder().create();
 
+    protected void sendJson(HttpExchange exchange, int statusCode, Object response) throws IOException {
+        String jsonResponse = gson.toJson(response);
+        byte[] bytes = jsonResponse.getBytes(StandardCharsets.UTF_8);
+        
+        exchange.getResponseHeaders().set("Content-Type", CT_JSON);
+        exchange.sendResponseHeaders(statusCode, bytes.length);
+        
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(bytes);
+        }
+    }
+
+    protected void sendNoContent(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(204, -1);
+    }
+
+    protected void sendError(HttpExchange exchange, int statusCode, String message) throws IOException {
+        ErrorResponse error = new ErrorResponse(message);
+        sendJson(exchange, statusCode, error);
+    }
 }
