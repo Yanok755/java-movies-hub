173ae7a5fdbd8a66c7fbfc7a865f84baab19faaf
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 18:25:38 +0300

    Update MoviesStore.java

--- src/main/ru/practicum/moviehub/store/MoviesStore.java
+++ src/main/ru/practicum/moviehub/store/MoviesStore.java
@@ -1,4 +1,25 @@
 package ru.practicum.moviehub.store;
 
+import ru.practicum.moviehub.model.Movie;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoviesStore {
+    private final List<Movie> movies = new ArrayList<>();
+
+    public List<Movie> getAllMovies() {
+        return new ArrayList<>(movies); // Возвращаем копию
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public void clear() {
+        movies.clear();
+    }
+
+    public int size() {
+        return movies.size();
+    }
 }
