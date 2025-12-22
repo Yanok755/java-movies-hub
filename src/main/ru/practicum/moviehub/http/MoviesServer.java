70fe1cddb883d07dc3b387e52d1c99d19d6bcac9
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 18:28:13 +0300

    Update MoviesServer.java

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,5 +1,32 @@
 package ru.practicum.moviehub.http;
 
-public class MoviesServer {
+import com.sun.net.httpserver.HttpServer;
+import ru.practicum.moviehub.store.MoviesStore;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class MoviesServer {
+    private final HttpServer server;
+    private final int port;
+
+    public MoviesServer(MoviesStore moviesStore, int port) {
+        this.port = port;
+        try {
+            this.server = HttpServer.create(new InetSocketAddress(port), 0);
+            this.server.createContext("/movies", new MoviesHandler(moviesStore));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create HTTP server", e);
+        }
+    }
+
+    public void start() {
+        server.start();
+        System.out.println("MovieHub server started on port " + port);
+    }
+
+    public void stop() {
+        server.stop(0);
+        System.out.println("MovieHub server stopped");
+    }
 }
