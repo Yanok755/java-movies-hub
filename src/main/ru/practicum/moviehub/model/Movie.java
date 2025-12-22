dc420110a2a95813eae543c0c1a5325106b67924
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-22 19:42:05 +0300

    Update Movie.java

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,78 +1,18 @@
 package ru.practicum.moviehub.model;
 
-import java.util.Objects;
-
 public class Movie {
-    private int id;
+    // Поля можно будет добавить позже
     private String title;
     private int year;
     private int duration;
-
-    public Movie() {
-        // Пустой конструктор для Gson
-    }
-
-    public Movie(int id, String title, int year, int duration) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.duration = duration;
-    }
-
-    // Getters и Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Movie movie = (Movie) o;
-        return id == movie.id && year == movie.year && duration == movie.duration
-                && Objects.equals(title, movie.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, year, duration);
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", year=" + year +
-                ", duration=" + duration +
-                '}';
-    }
+    
+    // Геттеры и сеттеры
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
 }
