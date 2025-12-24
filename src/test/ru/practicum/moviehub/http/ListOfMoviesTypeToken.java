f83fc14084a439108da41eade0416caf2e626f3a
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-24 23:40:57 +0300

    Update ListOfMoviesTypeToken.java

--- src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
+++ src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
@@ -1,8 +1,18 @@
 package ru.practicum.moviehub.http;
 
 import com.google.gson.reflect.TypeToken;
+import ru.practicum.moviehub.model.Movie;
+
+import java.lang.reflect.Type;
 import java.util.List;
 
-public class ListOfMoviesTypeToken extends TypeToken<List<Object>> {
-    // Пустой класс, используется только для определения типа
+public class ListOfMoviesTypeToken extends TypeToken<List<Movie>> {
+
+    public static Type getType() {
+        return new ListOfMoviesTypeToken().getType();
+    }
+
+    public static TypeToken<List<Movie>> getTypeToken() {
+        return new ListOfMoviesTypeToken();
+    }
 }
