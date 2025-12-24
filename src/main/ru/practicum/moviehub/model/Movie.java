4094777c614a9fbaba3f648d5ffa86e17d84f815
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-24 23:09:41 +0300

    Update Movie.java

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -1,18 +1,26 @@
 package ru.practicum.moviehub.model;
 
 public class Movie {
-    // Поля можно будет добавить позже
     private String title;
     private int year;
     private int duration;
-    
+
+    // Конструктор по умолчанию (нужен для Gson)
+    public Movie() {}
+
+    public Movie(String title, int year, int duration) {
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+
     // Геттеры и сеттеры
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-    
+
     public int getYear() { return year; }
     public void setYear(int year) { this.year = year; }
-    
+
     public int getDuration() { return duration; }
     public void setDuration(int duration) { this.duration = duration; }
 }
