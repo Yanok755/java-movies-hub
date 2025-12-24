b0e177a5846ad9bdb55c09fae9600f02eee1f81c
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-24 23:11:11 +0300

    Update MovieHubApp.java

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -6,7 +6,13 @@ import ru.practicum.moviehub.store.MoviesStore;
 public class MovieHubApp {
     public static void main(String[] args) {
         final MoviesServer server = new MoviesServer(new MoviesStore(), 8080);
-        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+
+        // Добавляем хук для корректного завершения приложения
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down...");
+            server.stop();
+        }));
+
         server.start();
     }
 }
