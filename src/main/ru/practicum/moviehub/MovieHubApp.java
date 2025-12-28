5d1c39226d38389852426b451c76981774eaa1bf
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-12-28 20:08:23 +0300

    Update MovieHubApp.java

--- src/main/ru/practicum/moviehub/MovieHubApp.java
+++ src/main/ru/practicum/moviehub/MovieHubApp.java
@@ -1,33 +1,13 @@
 package ru.practicum.moviehub;
 
 import ru.practicum.moviehub.http.MoviesServer;
-import ru.practicum.moviehub.store.MoviesStore;
+
+import java.io.IOException;
 
 public class MovieHubApp {
-    public static void main(String[] args) {
-        // Порт по умолчанию
-        int port = 8080;
-
-        // Можно указать порт через аргументы командной строки
-        if (args.length > 0) {
-            try {
-                port = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid port number: " + args[0]);
-                System.err.println("Using default port: 8080");
-            }
-        }
-
-        // Создаем и запускаем сервер
-        final MoviesServer server = new MoviesServer(new MoviesStore(), port);
-
-        // Добавляем хук для корректного завершения
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("\nShutting down MovieHub server...");
-            server.stop();
-        }));
-
-        // Запускаем сервер
+    public static void main(String[] args) throws IOException {
+        final MoviesServer server = new MoviesServer();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
         server.start();
     }
 }
